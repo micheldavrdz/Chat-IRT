64bf660d7de3dfce744d022c0c12730e0bc81399
Michel Rodriguez <micheldavrdz@gmail.com>
2022-02-15 05:31:41 -0600

    add: incoming messages
    App listens to send messages and shows them on screen

---
+++ public/js/main.js
@@ -0,0 +1,41 @@
+const formulario = document.getElementById('chat-form');
+const mensajesCanal = document.querySelector('.chat-messages');
+
+const socket = io();
+
+socket.on('mensaje', mensaje => {
+    console.log(mensaje);
+    enviarMensaje(mensaje);
+
+    // Hacer scroll al final del canal
+    mensajesCanal.scrollTop = mensajesCanal.scrollHeight;
+})
+
+// Enviar mensaje al canal
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Obtener el texto del mensaje enviado
+    const msg = e.target.elements.msg.value;
+
+    // Enviar mensaje al server
+    socket.emit('mensajeCanal', msg);
+
+    // Limpiar la caja de mensaje
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+});
+
+// Enviar mensaje al DOM
+function enviarMensaje(mensaje) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `
+        <p class="meta">Usuario <span>4:20pm</span></p>
+        <p class="text">
+            ${mensaje}
+        </p>
+    `;
+
+    mensajesCanal.appendChild(div);
+}
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Declaracion requerimentos
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
 const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,12 +11,31 @@ const io = socketIO(server);
 // Declarando el static dir de la aplicacion usando path
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Crear un log cada vez que se conecte un cliente
-io.on('conexion', (socket) => {
-    console.log('Nueva conexion: ', socket.id);
+// Crear un mensaje cada vez que se conecte un cliente
+
+// socket.emit es para enviar el mensaje al usuario que se conecto
+// socket.broadcast es para enviar el mensaje a todos los usuarios excepto al que se conecto
+// io.emit es para enviar el mensaje a TODOS los usuarios
+io.on('connection', (socket) => {
+
+    // Mensaje de bienvenida al usuario
+    socket.emit('mensaje', 'Bienvenid@ al chat');
+
+    // Mensaje cuando un usuario se conecta
+    socket.broadcast.emit('mensaje', 'Un usuario se ha conectado al canal');
+
+    // Mensaje cuando un usuario se desconecta
+    socket.on('disconnect', () => {
+        io.emit('mensaje', 'Un usuario se ha desconectado del canal');
+    });
+
+    // Esperar mensajes del chat (mensaje_canal)
+    socket.on('mensajeCanal', (msg) => {
+        io.emit('mensaje', msg);
+    });
 });
 
 // Declarando el puerto de la aplicacion
-const app_port = 3000 || process.env.app_port;
+const appPort = 3000 || process.env.appPort;
 
-server.listen(app_port, () => console.log(`El servidor esta corriendo en el puerto ${app_port}`));
+server.listen(appPort, () => console.log(`El servidor esta corriendo en el puerto ${appPort}`));
